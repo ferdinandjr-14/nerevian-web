3d26f2df635fac6ab6742df461554b98172ed9b9
Ferdinand <127405902+ferdinandjr-14@users.noreply.github.com>
2026-04-11 01:41:03 +0200

    Create Boat.vue

---
+++ src/components/Boat.vue
@@ -0,0 +1,25 @@
+<template>
+    <img :src="boatIcon" :alt="alt" :width="size" :height="size" class="boat-icon" />
+</template>
+
+<script setup>
+import boatIcon from "../assets/boat.svg"
+
+defineProps({
+    size: {
+        type: [Number, String],
+        default: 32,
+    },
+    alt: {
+        type: String,
+        default: "Boat",
+    },
+})
+</script>
+
+<style scoped>
+.boat-icon {
+    display: inline-block;
+    vertical-align: middle;
+}
+</style>
